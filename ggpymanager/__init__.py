8e05f6958415233fa133610ba6bb2f2aae35a9a7
Robert Maiwald <robert.maiwald@uni-heidelberg.de>
2025-08-11 13:57:05 -0400

    fix: update import statements for consistency and clarity

--- ggpymanager/__init__.py
+++ ggpymanager/__init__.py
@@ -1,7 +1,8 @@
-from .catalog import Catalog, N_HEADER
-from .simulation import Simulation, Status, State
-from .reader import Reader
-from .read_gramm_geometry import read_ggeom_file
+from ggpymanager.catalog import Catalog, N_HEADER
+from ggpymanager.simulation import Simulation, Status, State
+from ggpymanager.reader import Reader
+from ggpymanager.read_gramm_geometry import read_ggeom_file
+from ggpymanager import utils
 
 __all__ = [
     "Reader",
@@ -11,4 +12,5 @@ __all__ = [
     "State",
     "N_HEADER",
     "read_ggeom_file",
+    "utils",
 ]

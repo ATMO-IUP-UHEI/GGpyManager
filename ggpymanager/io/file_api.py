4b15b678f50a9845d9a4078a850ddfdb65de82d6
Robert Maiwald <robert.maiwald@uni-heidelberg.de>
2026-02-25 14:29:23 +0100

    refactor: streamline config usage in load function for clarity

--- ggpymanager/io/file_api.py
+++ ggpymanager/io/file_api.py
@@ -1,34 +1,31 @@
-import xarray as xr
+import logging
 from pathlib import Path
 
-from ggpymanager.config import (
-    STATUS_LOG_FILE_NAME,
-    MATCHING_LOSS_FILE_NAME,
-    CONCENTRATION_TIMESERIES_FILE_NAME,
-    GRAMM_METEO_TIMESERIES_FILE_NAME,
-    GRAL_METEO_TIMESERIES_FILE_NAME,
-)
+import xarray as xr
+
+from ggpymanager.config import (CONCENTRATION_TIMESERIES_FILE_NAME,
+                                GRAL_METEO_TIMESERIES_FILE_NAME,
+                                GRAMM_METEO_TIMESERIES_FILE_NAME,
+                                MATCHING_LOSS_FILE_NAME, STATUS_LOG_FILE_NAME)
 
 
 def load(data_name: str, config: dict) -> xr.Dataset:
+    c = config
     file_paths = {
         # Measurements
-        "temperature": Path(config["data_path"])
-        / config["meteo_path"]
-        / "temperature.nc",
-        "pressure": Path(config["data_path"]) / config["meteo_path"] / "pressure.nc",
+        "temperature": Path(c["data_path"]) / c["meteo_path"] / "temperature.nc",
+        "pressure": Path(c["data_path"]) / c["meteo_path"] / "pressure.nc",
         # Catalog
-        "gramm_status": Path(config["domain"]["gramm"]["conf_path"])
-        / STATUS_LOG_FILE_NAME,
-        "gral_status": Path(config["domain"]["gral"]["conf_path"])
-        / STATUS_LOG_FILE_NAME,
+        "gramm_status": Path(c["domain"]["gramm"]["conf_path"]) / STATUS_LOG_FILE_NAME,
+        "gral_status": Path(c["domain"]["gral"]["conf_path"]) / STATUS_LOG_FILE_NAME,
         # Outputs
-        "matching_loss": Path(config["output_path"]) / MATCHING_LOSS_FILE_NAME,
-        "concentration_timeseries": Path(config["gral_co2_path"])
+        "matching_loss": Path(c["output_path"]) / MATCHING_LOSS_FILE_NAME,
+        "concentration_timeseries": Path(c["gral_co2_path"])
         / CONCENTRATION_TIMESERIES_FILE_NAME,
-        "gramm_meteo_timeseries": Path(config["output_path"])
+        "gramm_meteo_timeseries": Path(c["output_path"])
         / GRAMM_METEO_TIMESERIES_FILE_NAME,
-        "gral_meteo_timeseries": Path(config["output_path"])
+        "gral_meteo_timeseries": Path(c["output_path"])
         / GRAL_METEO_TIMESERIES_FILE_NAME,
     }
+    logging.info(f"Opening {data_name} from {file_paths[data_name]}")
     return xr.open_dataset(file_paths[data_name])

a9d6beef471c76bb03c35d18ba21b035ad1ee217
Robert Maiwald <robert.maiwald@uni-heidelberg.de>
2026-02-25 14:57:49 +0100

    refactor: improve code formatting and enhance docstring for load function

--- ggpymanager/io/file_api.py
+++ ggpymanager/io/file_api.py
@@ -3,13 +3,32 @@ from pathlib import Path
 
 import xarray as xr
 
-from ggpymanager.config import (CONCENTRATION_TIMESERIES_FILE_NAME,
-                                GRAL_METEO_TIMESERIES_FILE_NAME,
-                                GRAMM_METEO_TIMESERIES_FILE_NAME,
-                                MATCHING_LOSS_FILE_NAME, STATUS_LOG_FILE_NAME)
+from ggpymanager.config import (
+    CONCENTRATION_TIMESERIES_FILE_NAME,
+    GRAL_METEO_TIMESERIES_FILE_NAME,
+    GRAMM_METEO_TIMESERIES_FILE_NAME,
+    MATCHING_LOSS_FILE_NAME,
+    STATUS_LOG_FILE_NAME,
+)
 
 
 def load(data_name: str, config: dict) -> xr.Dataset:
+    """Load a dataset based on the provided data name and configuration.
+
+    Parameters
+    ----------
+    data_name : str
+        The name of the dataset to load. Must be one of "temperature", "pressure",
+        "gramm_status", "gral_status", "matching_loss", "concentration_timeseries",
+        "gramm_meteo_timeseries", or "gral_meteo_timeseries".
+    config : dict
+        The configuration dictionary containing paths to the datasets.
+
+    Returns
+    -------
+    xr.Dataset
+        The loaded dataset corresponding to the provided data name.
+    """
     c = config
     file_paths = {
         # Measurements
